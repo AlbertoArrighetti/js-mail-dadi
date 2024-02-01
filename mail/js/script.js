f3e24b536bc4d11f515aa9d2ea65eaa4f00f0b40
Alberto-Arrighetti <alberto.arrighetti1571@gmail.com>
2024-02-01 18:06:39 +0100

    verifica array

--- mail/js/script.js
+++ mail/js/script.js
@@ -6,18 +6,41 @@
 // stampa un messaggio appropriato sull’esito del controllo.
 // (NON utilizzate il metodo .includes())
 
+
+
+
 // array con mail per poter accedere
 const mails = ["mario.rossi@gmail.com", "elia.barbieri@gmail.com", "vincenzo.montanari@gmail.com", "filippo.russo@gmail.com", "luca.rossi@gmail.com"]
 console.log(mails);
-
-// form per chiedere la mail all'utente
 
 // salvo il bottone
 const btnElement = document.getElementById("btn");
 // creo funzione per il click
 btnElement.addEventListener("click",
     function() {
-        const emailElement = document.getElementById("email").value ;
+        // contenuto inserito dall'utente
+        const emailElement = document.getElementById("email").value;
         console.log(emailElement);
+        
+        // creo una flag
+        let flag = false;
+        // creo un ciclo per array
+        for (let i = 0; i < mails.length; i++) {
+            
+            // verifico se la value di emailElement è all'interno dell'array
+            if (emailElement == mails[i]){
+                // se la email è corretta
+
+                flag = "email corretta";
+                console.log(flag);
+
+                // se la email non è corretta 
+            } else if (emailElement != mails[i]){
+
+                flag = "no";
+                console.log(flag);
+            }
+
+        }
     }
 )
